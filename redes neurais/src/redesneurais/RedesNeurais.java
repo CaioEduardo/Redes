1678b82500678cdce8ba94c834659419f06b1a92
CaioEduardo <caioalmom77@gmail.com>
2012-12-14 15:12:09 -0200

    TrampoIAParte1

---
+++ redes neurais/src/redesneurais/RedesNeurais.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package redesneurais;
+
+/**
+ *
+ * @author Caio
+ */
+public class RedesNeurais {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Janela().setVisible(true);
+        
+           }
+}
